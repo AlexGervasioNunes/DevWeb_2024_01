be924773a65338d7585ef1cd4f455f175d87312c
ALEX GERVASIO NUNES <agervasionunes@gmail.com>
2024-05-06 20:01:51 -0300

    m

---
+++ JAVASCRIPT/DESVIOS CONDICIONAIS/js/main.js
@@ -0,0 +1,17 @@
+const a = 10;
+if (a == 10) {
+  console.log("a = 10");
+}
+const b = "25";
+if (b == 25) {
+  console.log("b = 25");
+}
+const c = 9;
+if (c === 9) {
+  console.log("c = 9");
+}
+const d = "35";
+if (d === 35) {
+  console.log("b = 35");
+}
+console.log("I´m outside if, who compares string.");
