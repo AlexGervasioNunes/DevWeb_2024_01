a064d2bec492752ed667cec13085c10dfdab5237
ALEX GERVASIO NUNES <agervasionunes@gmail.com>
2024-05-08 19:13:27 -0300

    m

--- JAVASCRIPT/DESVIOS CONDICIONAIS/js/main.js
+++ JAVASCRIPT/DESVIOS CONDICIONAIS/js/main.js
@@ -2,16 +2,45 @@ const a = 10;
 if (a == 10) {
   console.log("a = 10");
 }
+
 const b = "25";
 if (b == 25) {
   console.log("b = 25");
 }
+
 const c = 9;
 if (c === 9) {
-  console.log("c = 9");
+  console.log('c = 9');
 }
-const d = "35";
+
+const d = '35';
 if (d === 35) {
-  console.log("b = 35");
+  console.log('b = 35');
 }
-console.log("I´m outside if, who compares string.");
+
+console.log('I´m outside if, who compares string.');
+
+const s ='2';
+if (s === 2) console.log('s is the number 10');
+
+else {
+  console.log ('Result: ${s ===10}');
+  console.log ('It is not a numeral.');
+}
+
+console.clear();
+
+const f = 450;
+const g = 555;
+
+if (f > 450 || g > 555){
+  console.log('Condition is ${ (f > 450 || g > 555}');
+  console.log('f is bigger than 450 or g is bigger than 555');}
+
+  else
+  console.log('f is not bigger than 450 or g is bigger than 555');
+if (f > 450 && g > 555)console.log('f is smaller than 450 and g is bigger than 555');
+
+else{
+  console.log('The condition is ${f > 450 && g > 555}');
+  console.log('f is not bigger than 450 or g is not bigger than 555');}
---
+++ JAVASCRIPT/LAÇOS DE REPETIÇÃO/js/main.js
@@ -0,0 +1,18 @@
+
+console.log('       - - - / / / - - -')
+console.log('      NÚMEROS. Do 1 ao 50.')
+console.log('       - - - / / / - - -')
+let contador = 1;
+while (contador <= 50) {
+    console.log(`Número ${contador}`);
+    contador++;
+}
+console.log('       - - - / / / - - -')
+console.log('CONTAGEM REGRESSIVA, de 10 a 0.')
+console.log('       - - - / / / - - -')
+let cont = 10;
+while (cont >= 0) {
+    console.log(`Valor = ${cont}`);
+    cont--;
+}
+for (let i = 100; i > 0; i--) console.log(`Laço For número: ${i}`);
