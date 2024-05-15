8a9eebd6631373fe047fe7be8549c31540380517
ALEX GERVASIO NUNES <agervasionunes@gmail.com>
2024-05-14 21:22:02 -0300

    m

--- JAVASCRIPT/OBJETOS/js/main.js
+++ JAVASCRIPT/OBJETOS/js/main.js
@@ -1,18 +1,134 @@
-let pessoa = {
-    firstName: 'Aristarco',
-    lastName: 'de Samos',
-    idade: 80,
-    corDosOlhos: 'preto',
-    hobbies: [`Astrônomo, Matemático`],
-    endereco: {
-        rua: 'Samos, Grécia',
-        numero: 0,
-    cidade: 'Samos',
-    país: 'Grécia',
+let alunos = {
+    zack: {
+        name: `Zack`,
+        age: `16`,
+        class: `1ºD`,
+        notas: {
+            matematica: `9.5`,
+            portugues: `7.5`,
+            historia: `8`,
+            biologia: `6.5`,
+            educacaoFisica: `9`,
+            quimica: `6.5`,
+            geografia: `8.5`,
+            fisica: `5`,
+            tecnologia: `6`,
+        },
+        endereco: {
+            city: `Sorocaba`,
+            state: `SP`,
+        },
+        media: `7.5`,
+        status: `APROVADO`,
     },
-    };
-    console.log(pessoa);
-    console.log(pessoa.firstName);
-    console.log(pessoa.lastName, pessoa.idade);
-    console.log(pessoa.hobbies[0]);
-    console.log(pessoa.endereco.cidade);
+    william: {
+        name: `William`,
+        age: `15`,
+        class: `1ºC`,
+        notas: {
+            matematica: `7.5`,
+            portugues: `6.5`,
+            historia: `7.5`,
+            biologia: `5`,
+            educacaoFisica: `5`,
+            quimica: `2`,
+            geografia: `7`,
+            fisica: `6`,
+            tecnologia: `9`,
+        },
+        endereco: {
+            city: `Guarulhos`,
+            state: `SP`,
+        },
+        media: `6`,
+        status: `REPROVADO`,
+    },
+    jaqueline: {
+        name: `Jaqueline`,
+        age: `17`,
+        class: `3ºA`,
+        notas: {
+            matematica: `10`,
+            portugues: `9.5`,
+            historia: `9.5`,
+            biologia: `5.5`,
+            educacaoFisica: `8`,
+            quimica: `8.5`,
+            geografia: `7.5`,
+            fisica: `10`,
+            tecnologia: `9`,
+        },
+        endereco: {
+            city: `Cerquilho`,
+            state: `SP`,
+        },
+        media: `8.5`,
+        status: `APROVADA`,
+    },
+};
+
+console.log(``);
+console.log(` - - - ALUNO 1 - - - `);
+console.log(``);
+
+console.log(`Nome: ${alunos.zack.name}`);
+console.log(`Idade: ${alunos.zack.age}`);
+console.log(`Série: ${alunos.zack.class}`);
+console.log(`Endereço: ${alunos.zack.endereco.city}, ${alunos.zack.endereco.state}`);
+console.log(``);
+console.log(`NOTAS`);
+console.log(`Matemática: ${alunos.zack.notas.matematica}`);
+console.log(`Português: ${alunos.zack.notas.portugues}`);
+console.log(`História: ${alunos.zack.notas.historia}`);
+console.log(`Biologia: ${alunos.zack.notas.biologia}`);
+console.log(`Educação Física: ${alunos.zack.notas.educacaoFisica}`);
+console.log(`Química: ${alunos.zack.notas.quimica}`);
+console.log(`Geografia: ${alunos.zack.notas.geografia}`);
+console.log(`Física: ${alunos.zack.notas.fisica}`);
+console.log(`Tecnologia: ${alunos.zack.notas.tecnologia}`);
+console.log(`Média: ${alunos.zack.media}`);
+console.log(`Série: ${alunos.zack.status}`);
+
+console.log(``);
+console.log(` - - - ALUNO 2 - - - `);
+console.log(``);
+
+console.log(`Nome: ${alunos.william.name}`);
+console.log(`Idade: ${alunos.william.age}`);
+console.log(`Série: ${alunos.william.class}`);
+console.log(`Endereço: ${alunos.william.endereco.city}, ${alunos.william.endereco.state}`);
+console.log(``);
+console.log(`NOTAS`);
+console.log(`Matemática: ${alunos.william.notas.matematica}`);
+console.log(`Português: ${alunos.william.notas.portugues}`);
+console.log(`História: ${alunos.william.notas.historia}`);
+console.log(`Biologia: ${alunos.william.notas.biologia}`);
+console.log(`Educação Física: ${alunos.william.notas.educacaoFisica}`);
+console.log(`Química: ${alunos.william.notas.quimica}`);
+console.log(`Geografia: ${alunos.william.notas.geografia}`);
+console.log(`Física: ${alunos.william.notas.fisica}`);
+console.log(`Tecnologia: ${alunos.william.notas.tecnologia}`);
+console.log(`Média: ${alunos.william.media}`);
+console.log(`Série: ${alunos.william.status}`);
+
+console.log(``);
+console.log(` - - - ALUNO 3 - - - `);
+console.log(``);
+
+console.log(`Nome: ${alunos.jaqueline.name}`);
+console.log(`Idade: ${alunos.jaqueline.age}`);
+console.log(`Série: ${alunos.jaqueline.class}`);
+console.log(`Endereço: ${alunos.jaqueline.endereco.city}, ${alunos.jaqueline.endereco.state}`);
+console.log(``);
+console.log(`NOTAS`);
+console.log(`Matemática: ${alunos.jaqueline.notas.matematica}`);
+console.log(`Português: ${alunos.jaqueline.notas.portugues}`);
+console.log(`História: ${alunos.jaqueline.notas.historia}`);
+console.log(`Biologia: ${alunos.jaqueline.notas.biologia}`);
+console.log(`Educação Física: ${alunos.jaqueline.notas.educacaoFisica}`);
+console.log(`Química: ${alunos.jaqueline.notas.quimica}`);
+console.log(`Geografia: ${alunos.jaqueline.notas.geografia}`);
+console.log(`Física: ${alunos.jaqueline.notas.fisica}`);
+console.log(`Tecnologia: ${alunos.jaqueline.notas.tecnologia}`);
+console.log(`Média: ${alunos.jaqueline.media}`);
+console.log(`Série: ${alunos.jaqueline.status}`);
